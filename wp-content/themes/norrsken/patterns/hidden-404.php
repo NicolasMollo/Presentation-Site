2711bf83f4825c6c1a6fcaac993837572506ab54
Nicolas Mollo <nicolasmollo.98@gmail.com>
2024-08-01 18:25:23 +0200

    Added new static site files

---
+++ wp-content/themes/norrsken/patterns/hidden-404.php
@@ -0,0 +1,10 @@
+
+<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"heading-3"} -->
+
+<h1 class="wp-block-heading has-text-align-center has-heading-3-font-size" id="page-not-found">
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function esc_attr_x() in C:\xampp\htdocs\Projects\curriculum_nicolasmollo\wp-content\themes\norrsken\patterns\hidden-404.php:10
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\Projects\curriculum_nicolasmollo\wp-content\themes\norrsken\patterns\hidden-404.php</b> on line <b>10</b><br>
+</h1>
---
+++ wp-content/themes/norrsken/patterns/hidden-footer.php
@@ -0,0 +1,29 @@
+<p>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"},"blockGap":"var:preset|spacing|80"}},"layout":{"type":"constrained"}} -->
+</p>
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)">
+	<!-- wp:separator -->
+	<hr class="wp-block-separator has-alpha-channel-opacity">
+	<!-- /wp:separator -->
+
+	<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+	<div class="wp-block-group">
+		<!-- wp:group {"style":{"spacing":{"blockGap":".6em"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+		<div class="wp-block-group">
+<!-- wp:paragraph -->
+			<p>© 2024</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:site-title /-->
+		</div>
+		<!-- /wp:group -->
+
+		<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast-2"}}}},"textColor":"contrast-2","className":"theme-author-credit"} -->
+		<p class="has-text-align-center theme-author-credit has-contrast-2-color has-text-color has-link-color"><br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function esc_url() in C:\xampp\htdocs\Projects\curriculum_nicolasmollo\wp-content\themes\norrsken\patterns\hidden-footer.php:30
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\Projects\curriculum_nicolasmollo\wp-content\themes\norrsken\patterns\hidden-footer.php</b> on line <b>30</b><br>
+</p>
+</div>
+</div>
